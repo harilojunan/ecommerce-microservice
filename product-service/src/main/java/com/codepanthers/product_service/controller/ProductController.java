2c242b57d1035c4a0417aae5c2c445cee998ba00
Harilojunan <hari23may@gmail.com>
2025-11-10 22:01:19 +0530

    added API url in constance and redis configuration in app.properties file

--- product-service/src/main/java/com/codepanthers/product_service/controller/ProductController.java
+++ product-service/src/main/java/com/codepanthers/product_service/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.codepanthers.product_service.controller;
 
 import com.codepanthers.product_service.dto.mapper.ProductMapper;
 import com.codepanthers.product_service.service.ProductService;
+import com.codepanthers.product_service.util.AppConstance;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1/products")
+@RequestMapping(AppConstance.BASE_URL)
 @RequiredArgsConstructor
 public class ProductController {
 
--- product-service/src/main/java/com/codepanthers/product_service/util/AppConstance.java
+++ product-service/src/main/java/com/codepanthers/product_service/util/AppConstance.java
@@ -1,4 +1,21 @@
 package com.codepanthers.product_service.util;
 
 public class AppConstance {
+
+    /** The Constant Cache HASH_VALUE. */
+    public static final String HASH_VALUE = "product";
+    /** The Constant Cache HASH_KEY. */
+    public static final String HASH_KEY = "#id";
+    /** The Constant Cache HASH_VALUE_KEY. */
+    public static final String HASH_VALUE_KEY = "#product.id";
+
+    /** The Constant CROSS_ORIGIN_URL. */
+    public static final String CROSS_ORIGIN_URL = "http://localhost:8081";
+    /** The Constant BASE_URL. */
+    public static final String BASE_URL = "/api/v1/products";
+
+    /** The Constant PRODUCTS_URL. */
+    public static final String PRODUCTS_URL = "/products";
+    /** The Constant PRODUCT_BY_ID_URL. */
+    public static final String PRODUCT_BY_ID_URL = "/product/{id}";
 }
